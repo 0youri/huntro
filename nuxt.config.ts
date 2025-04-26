6dd65840ab2c406bda90062699519d76d9b30a94
Youri Novikov <73738882+0youri@users.noreply.github.com>
2025-04-26 19:22:16 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'netlify'
+  },
   modules: [
     '@nuxt/fonts',
     '@nuxt/icon',
